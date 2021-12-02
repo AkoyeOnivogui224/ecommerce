07ae43d3e6247528f21882b8e3325fb700228fd3
Akoye Onivogui <ema3onivoguieao@gmail.com>
2021-12-02 14:25:52 +0000

    Mise en place du paiment avec L'API stripe

---
+++ public/js/payment.js
@@ -0,0 +1,55 @@
+// Permet l'écoute du chargement du DOM
+window.addEventListener('DOMContentLoaded', (event) => {
+
+
+    const stripe = Stripe(stripePublicKey)
+    const elements = stripe.elements({ clientSecret });
+
+
+    const paymentElement = elements.create("payment");
+
+    paymentElement.mount("#payment-element");
+
+    document.querySelector('#payment-form').addEventListener('submit', async e => {
+
+        e.preventDefault()
+
+        const { error } = await stripe.confirmPayment({
+            elements,
+            confirmParams: {
+                return_url: redirectAfterSuccessUrl
+            }
+        });
+
+        if (error.type === "card_error" || error.type === "validation_error") {
+
+            showMessage(error.message)
+
+        } else {
+
+            showMessage("An unexpected error occured.");
+
+        }
+
+    })
+});
+
+function showMessage(messageText) {
+
+    const messageContainer = document.querySelector("#payment-message");
+
+
+    messageContainer.classList.remove("hidden");
+
+    messageContainer.textContent = messageText;
+
+
+    setTimeout(function () {
+
+        messageContainer.classList.add("hidden");
+
+        messageText.textContent = "";
+
+    }, 4000);
+
+}
--- src/Controller/Purchase/PurchaseConfirmationController.php
+++ src/Controller/Purchase/PurchaseConfirmationController.php
@@ -5,29 +5,25 @@ namespace App\Controller\Purchase;
 use DateTime;
 use App\Entity\Purchase;
 use App\Cart\CartService;
-use App\Entity\PurchaseItem;
 use App\Form\CartConfirmationType;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Component\HttpFoundation\Request;
-use Symfony\Component\Security\Core\Security;
-use Symfony\Component\Routing\RouterInterface;
+use App\Purchase\PurchasePersister;
 use Symfony\Component\Routing\Annotation\Route;
-use Symfony\Component\Form\FormFactoryInterface;
-use Symfony\Component\HttpFoundation\RedirectResponse;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\IsGranted;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-use Symfony\Component\Security\Core\Exception\AccessDeniedException;
-use Symfony\Component\HttpFoundation\Session\Flash\FlashBagInterface;
 
 class PurchaseConfirmationController extends AbstractController
 {
     protected $cartService;
     protected $em;
+    protected $persister;
 
-    public function __construct(CartService $cartService, EntityManagerInterface $em)
+    public function __construct(CartService $cartService, EntityManagerInterface $em, PurchasePersister $persister)
     {
         $this->cartService = $cartService;
         $this->em = $em;
+        $this->persister = $persister;
     }
 
     /**
@@ -59,29 +55,10 @@ class PurchaseConfirmationController extends AbstractController
         /** @var Purchase */
         $purchase = $form->getData();
 
-        $purchase->setUser($user)
-            ->setPurchasedAt(new DateTime())
-            ->setTotal($this->cartService->getTotal());
+        $this->persister->storePurchase($purchase);
 
-        $this->em->persist($purchase);
-
-        foreach ($this->cartService->getDetailedCartItem() as $cartItem) {
-            $purchaseItem = new PurchaseItem;
-            $purchaseItem->setPurchase($purchase)
-                ->setProduct($cartItem->product)
-                ->setProductName($cartItem->product->getName())
-                ->setProductPrice($cartItem->product->getPrice())
-                ->setQuantity($cartItem->qty)
-                ->setTotal($cartItem->getTotal());
-
-            $this->em->persist($purchaseItem);
-        }
-
-        $this->em->flush();
-        $this->cartService->empty();
-
-        $this->addFlash('success', "La commande a bien été enregistré");
-
-        return $this->redirectToRoute('purchase_index');
+        return $this->redirectToRoute('purchase_payment_form', [
+            'id' => $purchase->getId()
+        ]);
     }
 }
